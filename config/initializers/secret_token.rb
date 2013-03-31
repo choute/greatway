4cd8e6f02f735b56d20d820f89008a53108d068b
Cliff Choute <choute@gmail.com>
2013-03-31 17:24:54 -0400

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greatway::Application.config.secret_token = '611a1981b305cca94528b522a06b193d8ed56f1f17c8bf93bcc3371a1033a050d57ca72b53dceeecbdb72d33263bad9ec1670d02d5cf19cd0669aacec895f5d0'
